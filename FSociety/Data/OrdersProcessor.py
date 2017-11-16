18510ad34203712828e5d459512ee3264a542d9f
javier.bejar <bejar@cs.upc.edu>
2017-11-16 11:51:45 +0100

    Reworking the code

--- FSociety/Data/OrdersProcessor.py
+++ FSociety/Data/OrdersProcessor.py
@@ -6,7 +6,7 @@ StockOrders
 
 :Description: StockOrders
 
-    
+    Procesa ordenes y las guarda en un estructuras de datos
 
 :Authors: bejar
     
@@ -107,67 +107,67 @@ class OrdersProcessor:
                 self.orders[order.id].size -= order.size  # Modify the size of the order
                 self.orders[order.id].history.append(('X', order.otime, order.size))
 
-    def process_order(self, stock, order, id, otime=None, bos=None, updid=None, price=None, size=None):
-        """
-        Inserts an order in the structure
-
-        :param stock: Nombre de la accion
-        :param otime: Tiempo orden en ITCHtime
-        :param bos:  Compra o venta (B/S)
-        :param updid: Actualizacion del ID de la orden (para modificaciones)
-        :param price: precio de la orden
-        :param size: Tamanyo de la orden
-        :param order: Tipo de orden
-        :param id: Identificador de la orden
-        :return:
-        """
-
-        # Order Add (B/S)
-        if order in ['A', 'F']:
-            self.orders[id] = Order(order, id, otime, stock=stock, b_s=bos, price=price, size=size)
-
-        # Order Executed (total or partial)
-        if order in ['E']:
-            if self.history:
-                # Modify the size of the order
-                self.orders[id].size -= size
-                # Add to the history of executions of the order its execution
-                self.orders[id].history.append(('E', otime, size))
-                # If no shares left, move it to executed
-                if self.orders[id].size == 0:
-                    self.executed[id] = self.orders.pop(id)
-
-        # Order Executed (total or partial) with price
-        if order in ['C']:
-            if id in self.orders:
-                # Modify the size of the order
-                self.orders[id].size -= size
-                # Add to the history of executions of the order its execution
-                self.orders[id].history.append(('C', otime, size, price))
-                # If no shares left, move it to executed
-                if self.orders[id].size == 0:
-                    self.executed[id] = self.orders.pop(id)
-            else:
-                print('Order vanished')
-
-        # Order Replace (cancel+replace)
-        if order == 'U':
-            # Add a new order with the new parameters
-            self.orders[id] = Order(order, id, otime, stock=stock, b_s=self.orders[updid].buy_sell, price=price, size=size)
-            # Delete the original order from active orders
-            ro = self.orders.pop(updid)
-            # Add the history of the original order
-            self.orders[id].history = ro.history + self.orders[id].history
-
-        # Delete Order
-        if order == 'D' and id in self.orders:
-            self.canceled[id] = self.orders.pop(id)
-            self.canceled[id].history.append(('D', otime))
-
-        # Partial cancelation
-        if order == 'X' and id in self.orders:
-            self.orders[id].size -= size  # Modify the size of the order
-            self.orders[id].history.append(('X', otime, size))
+    # def process_order(self, stock, order, id, otime=None, bos=None, updid=None, price=None, size=None):
+    #     """
+    #     Inserts an order in the structure
+    #
+    #     :param stock: Nombre de la accion
+    #     :param otime: Tiempo orden en ITCHtime
+    #     :param bos:  Compra o venta (B/S)
+    #     :param updid: Actualizacion del ID de la orden (para modificaciones)
+    #     :param price: precio de la orden
+    #     :param size: Tamanyo de la orden
+    #     :param order: Tipo de orden
+    #     :param id: Identificador de la orden
+    #     :return:
+    #     """
+    #
+    #     # Order Add (B/S)
+    #     if order in ['A', 'F']:
+    #         self.orders[id] = Order(order, id, otime, stock=stock, b_s=bos, price=price, size=size)
+    #
+    #     # Order Executed (total or partial)
+    #     if order in ['E']:
+    #         if self.history:
+    #             # Modify the size of the order
+    #             self.orders[id].size -= size
+    #             # Add to the history of executions of the order its execution
+    #             self.orders[id].history.append(('E', otime, size))
+    #             # If no shares left, move it to executed
+    #             if self.orders[id].size == 0:
+    #                 self.executed[id] = self.orders.pop(id)
+    #
+    #     # Order Executed (total or partial) with price
+    #     if order in ['C']:
+    #         if id in self.orders:
+    #             # Modify the size of the order
+    #             self.orders[id].size -= size
+    #             # Add to the history of executions of the order its execution
+    #             self.orders[id].history.append(('C', otime, size, price))
+    #             # If no shares left, move it to executed
+    #             if self.orders[id].size == 0:
+    #                 self.executed[id] = self.orders.pop(id)
+    #         else:
+    #             print('Order vanished')
+    #
+    #     # Order Replace (cancel+replace)
+    #     if order == 'U':
+    #         # Add a new order with the new parameters
+    #         self.orders[id] = Order(order, id, otime, stock=stock, b_s=self.orders[updid].buy_sell, price=price, size=size)
+    #         # Delete the original order from active orders
+    #         ro = self.orders.pop(updid)
+    #         # Add the history of the original order
+    #         self.orders[id].history = ro.history + self.orders[id].history
+    #
+    #     # Delete Order
+    #     if order == 'D' and id in self.orders:
+    #         self.canceled[id] = self.orders.pop(id)
+    #         self.canceled[id].history.append(('D', otime))
+    #
+    #     # Partial cancelation
+    #     if order == 'X' and id in self.orders:
+    #         self.orders[id].size -= size  # Modify the size of the order
+    #         self.orders[id].history.append(('X', otime, size))
 
     def query_id(self, id):
         if id in self.orders:
--- Scripts/SelectExecutions.py
+++ Scripts/SelectExecutions.py
@@ -19,57 +19,57 @@ Executions
 
 from FSociety.Data import Company, OrdersProcessor
 from FSociety.Config import datapath, ITCH_days
-import argparse
+from FSociety.ITCH import ITCHMessages
 
+import argparse
+import gzip
 
 __author__ = 'bejar'
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--year', help="Anyo del analisis", default=None)
-    parser.add_argument('--day', help="numero de dia del anyo a analizar", default=None)
-    parser.add_argument('--stock', help="Ticker a analizar", default=None)
+    parser.add_argument('--year', help="Anyo del analisis", default='')
+    parser.add_argument('--day', help="dia del anyo", default='')
+    parser.add_argument('--stock', help="Stock del analisis", default='')
 
     args = parser.parse_args()
-    year = args.year
+    year = str(args.year)
     stock = args.stock
-    nday = int(args.day)
 
-    day = ITCH_days[year][nday]
+    if year == '':
+        year = '2017G'
+
+    if args.day == '':
+        day = 0
+    else:
+        day = int(args.day)
+
+    if stock == '':
+        stock = 'AAPL'
+
+    if 'G' in year:
+        datapath = datapath + '/GIS/'
+
+    rfile = ITCHMessages(year, day, stock)
     sorders = OrdersProcessor()
+    rfile.open()
+
     cpny = Company()
-    rfile = open(datapath + 'Messages/' + day + '-' + stock + '-MESSAGES.csv', 'r')
+
     wfile = open(datapath + '/Results/' + day + '-' + stock + '-EXEC.csv', 'w')
 
     i = 0
     norders = 0
-    for mess in rfile:
-        data = mess.split(',')
-        timestamp = int(data[1].strip())
-        order = data[2].strip()
-        ORN = data[3].strip()
-        if order in ['F', 'A']:
-            if order == 'A':
-                price = float(data[7].strip())
-            else:
-                price = float(data[8].strip())
-            sorders.process_order(stock, order, ORN, otime=timestamp.itime, bos=data[5].strip(), price=price)
-            norders += 1
-        if order == 'U':
-            nORN =  data[4].strip()
-            sorders.process_order(stock, order, nORN, timestamp.itime, updid=ORN, price=data[6].strip())
-        # Computes the time between placing and order and canceling it
-        if order == 'D':
-            trans = sorders.query_id(ORN)
-            sorders.process_order(stock, order, ORN)
-        # Computes the time between placing and order and its execution
-        if order in ['E', 'C']:
-            trans = sorders.query_id(ORN)
-            wfile.write(stock + ',' + str(timestamp) + ',' + data[4].strip() + ',' + str(trans.otime) + ',' + trans.buy_sell + ',' + str(trans.price))
+    for order in rfile.get_order():
+        print(order.to_string())
+        sorders.insert_order(order)
+        if order.type in ['E', 'C']:
+            trans = sorders.query_id(order.ORN)
+            wfile.write(stock + ',' + str(order.otimes) + ',' + str(trans.otime) + ',' + trans.buy_sell + ',' + str(order.size) + ',' + str(trans.price))
 
             if order == 'C':
-                 wfile.write(',' + str(data[6].strip()) + '\n')
+                 wfile.write(',' + str(order.price) + '\n')
             else:
                  wfile.write('\n')
 
@@ -80,3 +80,37 @@ if __name__ == '__main__':
 
     wfile.close()
     rfile.close()
+
+    # for mess in rfile:
+    #     data = mess.split('&')
+    #     timestamp = int(data[1].strip())
+    #     order = data[2].strip()
+    #     ORN = data[3].strip()
+    #     if order in ['F', 'A']:
+    #         price = float(data[7].strip())
+    #         sorders.process_order(stock, order, ORN, otime=timestamp.itime, bos=data[5].strip(), price=price)
+    #         # norders += 1
+    #     if order == 'U':
+    #         nORN =  data[4].strip()
+    #         sorders.process_order(stock, order, nORN, timestamp.itime, updid=ORN, price=data[6].strip())
+    #     # Computes the time between placing and order and canceling it
+    #     if order == 'D':
+    #         trans = sorders.query_id(ORN)
+    #         sorders.process_order(stock, order, ORN)
+    #     # Computes the time between placing and order and its execution
+    #     if order in ['E', 'C']:
+    #         trans = sorders.query_id(ORN)
+    #         wfile.write(stock + ',' + str(timestamp) + ',' + data[4].strip() + ',' + str(trans.otime) + ',' + trans.buy_sell + ',' + str(trans.price))
+    #
+    #         if order == 'C':
+    #              wfile.write(',' + str(data[6].strip()) + '\n')
+    #         else:
+    #              wfile.write('\n')
+    #
+    #     i += 1
+    #     if i % 10000 == 0:
+    #         print('.', end='', flush=True)
+    #         wfile.flush()
+    #
+    # wfile.close()
+    # rfile.close()
